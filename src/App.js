add7e27f0f818a7d96e22f07a53e81f538ef43da
Nick Hvattum <nhvattum@gmail.com>
2020-07-24 10:22:40 -0700

    setup router scaffolding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Signup from '/.conmponents/Signup'
+import Login from '/.conmponents/Login'
+import About from '/.conmponents/About'
+import Welcome from '/.conmponents/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="react-router-logic">
+        <Switch>
+          <Route path='/signup' component={ Signup } />
+          <Route path='/login' component={ Login } />
+          <Route path='/about' component={ About } />
+          <Route path='/' component={ Welcome } />
+        </Switch>
+      </div>
     </div>
   );
 }
